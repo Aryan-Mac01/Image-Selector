47c6dc6276481ede8ba4d9570fe39a6ff18e1a54
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2023-10-30 14:52:13 +0530

    fetchImages

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,97 @@
-import React, { useRef } from 'react';
-import { Form } from 'react-bootstrap';
+import axios from 'axios';
+import { useEffect, useRef, useState, useCallback } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import './index.css';
 
+const API_URL = 'https://api.unsplash.com/search/photos';
+const IMAGES_PER_PAGE = 20;
 
 const App = () => {
   const searchInput = useRef(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const fetchImages = useCallback(async () => {
+    try {
+      const { data } = await axios.get(
+        `${API_URL}?query=${
+          searchInput.current.value
+        }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+          import.meta.env.VITE_API_KEY
+        }`
+      );
+      console.log('data', data);
+      setImages(data.results);
+      setTotalPages(data.total_pages);
+    } catch (error) {
+      console.log(error);
+    }
+  },[page]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages, page]);
+
+  
+
+  const resetSearch = () => {
+    setPage(1);
+    fetchImages();
+  };
 
   const handleSearch = (event) => {
     event.preventDefault();
-    console.log("Submitted");
-  }
+    console.log(searchInput.current.value);
+    resetSearch();
+  };
+
+  const handleSelection = (selection) => {
+    searchInput.current.value = selection;
+    resetSearch();
+  };
+
+  console.log('page', page);
+
   return (
     <div className='container'>
-      <h1 className='title'>Image Selector</h1>
-        <div className='search-section'>
-          <Form>
-            <Form.Control
-              type='search'
-              placeholder='Type something to search...'
-              className='search-input'
-            />
-          </Form>
-        </div>
-      
+      <h1 className='title'>Image Search</h1>
+      <div className='search-section'>
+        <Form onSubmit={handleSearch}>
+          <Form.Control
+            type='search'
+            placeholder='Type something to search...'
+            className='search-input'
+            ref={searchInput}
+          />
+        </Form>
+      </div>
+      <div className='filters'>
+        <div onClick={() => handleSelection('nature')}>Nature</div>
+        <div onClick={() => handleSelection('birds')}>Birds</div>
+        <div onClick={() => handleSelection('cats')}>Cats</div>
+        <div onClick={() => handleSelection('shoes')}>Shoes</div>
+      </div>
+      <div className='images'>
+        {images.map((image) => (
+          <img
+            key={image.id}
+            src={image.urls.small}
+            alt={image.alt_description}
+            className='image'
+          />
+        ))}
+      </div>
+      <div className='buttons'>
+        {page > 1 && (
+          <Button onClick={() => setPage(page - 1)}>Previous</Button>
+        )}
+        {page < totalPages && (
+          <Button onClick={() => setPage(page + 1)}>Next</Button>
+        )}
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
